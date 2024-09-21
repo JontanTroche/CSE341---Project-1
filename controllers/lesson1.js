09d2824c2ef6c2ae9a34dedc40b5afc3ae997ca8
Jonatan Troche <trochealmediajonatan@gmail.com>
2024-09-21 02:31:08 -0400

    routes folder added

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -2,6 +2,16 @@ const nameRoute = (req, res) => {
     res.send('Jonatan Troche');
 };
 
+const valeRoute = (req, res) => {
+    res.send('Ruby Valentina');
+};
+
+const tathyRoute = (req, res) => {
+    res.send('tathy Troche');
+};
+
 module.exports = {
     nameRoute,
+    valeRoute,
+    tathyRoute,
 }
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-const lesson1Controllers = require('./controllers/lesson1');
-
-app.get('/', lesson1Controllers.nameRoute);
 
 const port = 3000;
+
+app.use('/', require('./routes'));
 
 app.listen(process.env.port || port);
 console.log('Web Server is listenning at port ' + (process.env.port || port));
